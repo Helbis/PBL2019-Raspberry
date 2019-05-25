5ceca5a1e08e043bdaf1afc86be0418408f100d7
Helbis <filiphelbig97@gmail.com>
2019-05-25 17:22:17 +0200

    Collection of hashing utilities

---
+++ myHashUtils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import hashlib
+
+
+def passToHash(password):
+    """ Those functions accept only byte arrays and output objects.
+        It is neccesary to use str.encode() to change from string to byte arrays
+        and hexdigest() to get from object to hex value."""
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+
+def compareHashAndGuess(hashed, guess):
+    if isinstance(guess, str):
+        if hashed == guess:
+            return True
+    elif isinstance(guess, dict):
+        result = []
+        keys = guess.keys()
+        values = guess.values()
+        for word in guess.values():
+            # print(hashed, word) # DEBUG: 
+            if str(word) == str(hashed):
+                result.append(True)
+            else:
+                result.append(False)
+        return result
+    else:
+        return False
+
+
+def genGuess():
+    return "5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8".lower()
