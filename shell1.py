6c6c71ec017e305aba59b184acfc85b242ac8df1
Helbis <filiphelbig97@gmail.com>
2019-05-27 23:10:47 +0200

    First attempt at getting some GUI for "shell" app
    It lacks everything that a shell should have but it may be enough to use it as a crack.py program launcher.

---
+++ shell1.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+
+def getString():
+    sTemp = entry.get()
+    label2["text"] = sTemp
+    print(f"sTemp : {sTemp}")
+
+
+root = Tk()
+
+sTemp = ""
+
+label = Label(root, text="Enter text: ")
+label2 = Label(root)
+entry = Entry(root)
+button = Button(root, text="Confirm", command=getString)
+
+label.grid(row=0, column=0)
+entry.grid(row=0, column=1)
+button.grid(row=1, column=0)
+label2.grid(row=1, column=1)
+
+root.mainloop()
